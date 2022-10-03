8a229e464d7589994b692e9d2ff5e86f9a124a6f
Alexis-Chata <alexizz.19.ac@gmail.com>
2022-10-02 23:58:20 -0500

    mejoras

--- resources/views/gestionar_usuarios.blade.php
+++ resources/views/gestionar_usuarios.blade.php
@@ -9,13 +9,6 @@
 @stop
 @section('content')
     @livewire('gestionar-usuarios')
-    @livewireScripts
-    <script>
-        window.livewire.on('notificar_seleccion', accion => {
-            document.getElementById("1_pestana").click();
-        });
-    </script>
-
 @stop
 
 @section('css')
@@ -27,4 +20,10 @@
 @section('js')
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+<script>
+    window.livewire.on('notificar_seleccion', accion => {
+        document.getElementById("1_pestana").click();
+    });
+</script>
 @stop
