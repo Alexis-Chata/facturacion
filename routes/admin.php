f08daee792f21d23f3f5c6ee8821e71cf43e3935
rudranthonny <ing.anthonny.joel@gmail.com>
2022-10-26 08:20:22 -0500

    correcion route

--- routes/admin.php
+++ routes/admin.php
@@ -14,5 +14,5 @@ Route::view('/reporte', 'administrador.recibos.recibos_reporte')->middleware('ca
 Route::view('usuario/cambiar', 'administrador.usuarios.cambiar_password')->middleware('can:admin.recibos')->name('admin.usuario.cambiar');
 //Route::view('/historial', 'historial_recibos')->middleware('can:admin.recibos')->name('admin.recibos');
 //Route::view('/usuarios', 'gestionar_usuarios');
-$formatter = new NumeroALetras();
-Route::view('/comprobante', 'recibos.comprobante_pdf',['recibo' => Recibo::find(4),'total_letra' => $formatter->toMoney(80, 2, 'QUETZALES', 'CENTAVOS')]);
+//$formatter = new NumeroALetras();
+//Route::view('/comprobante', 'recibos.comprobante_pdf',['recibo' => Recibo::find(4),'total_letra' => $formatter->toMoney(80, 2, 'QUETZALES', 'CENTAVOS')]);
