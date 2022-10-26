731d7320220815ce92d68067be8b508cc9d8d9a3
rudranthonny <ing.anthonny.joel@gmail.com>
2022-10-25 19:04:25 -0500

    correción 3

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,17 @@ class UserSeeder extends Seeder
         $usuarioadmin->materno = 'Espacio';
         $usuarioadmin->dpi="123456789";
         $usuarioadmin->email = 'administrador@arquitectura.com';
+        $usuarioadmin->password = bcrypt('Admin#123.@arquitectura.com');
+        $usuarioadmin->save();
+        $usuarioadmin->assignRole('Administrador');
+        $usuarioadmin->assignRole('Cajero');
+
+        $usuarioadmin = new User();
+        $usuarioadmin->name = 'Otoniel Calderón';
+        $usuarioadmin->paterno = 'Espacio';
+        $usuarioadmin->materno = 'Arquitectura ';
+        $usuarioadmin->dpi="123456789";
+        $usuarioadmin->email = 'grupoespacio@hotmail.es';
         $usuarioadmin->password = bcrypt('123456789');
         $usuarioadmin->save();
         $usuarioadmin->assignRole('Administrador');
